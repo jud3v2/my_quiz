22fe3a1124774a071cdaca1da040cffe5bfe8e43
jud3v2 <judikaelbellance@icloud.com>
2024-05-13 14:04:47 +0200

    ajout de la page d'acceuil avec l'affichage de la sélèction d'un quizz

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CategorieController extends AbstractController
 {
-    #[Route('/categorie', name: 'app_categorie')]
-    public function index(): Response
+    #[Route('/quizz/{id}', name: 'quizz')]
+    public function index(EntityManagerInterface $entityManager, Categorie $categorie): Response
     {
+            $questions = $entityManager->getRepository(QuestionController::class)
+            ->findBy(['id_categorie' => $categorie->getId()]);
         return $this->render('categorie/index.html.twig', [
-            'controller_name' => 'CategorieController',
+            'quizz' => $questions,
+                'questions' => $categorie->getQuestions(),
         ]);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +11,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('home/index.html.twig');
+            $categories = $entityManager->getRepository(Categorie::class)->findAll();
+        return $this->render('home/index.html.twig', [
+            'categories' => $categories,
+        ]);
     }
 }
