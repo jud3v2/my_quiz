a44b106cb1d890f0864697f4709f37a06cdbc260
jud3v2 <judikaelbellance@icloud.com>
2024-05-14 17:17:22 +0200

    ajout des FormBuilder pour handle les formulaires de l'application, de ce fait il est possible de réinitialiser le mot de passe, et de changer l'adresse email avec l'envoie d'un email de confirmation, lors du changement d'email un compte vérifié devient alors un compte non vérifié

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,13 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UpdateEmailFormType;
+use App\Form\UpdatePasswordFieldType;
+use App\Form\UpdateProfileFormType;
 use App\Security\EmailVerifier;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mime\Address;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class UserController extends AbstractController
@@ -33,7 +38,7 @@ class UserController extends AbstractController
     }
 
     #[Route('/user/profile', name: 'user.profile')]
-    public function profile()
+    public function profile(): Response
     {
             $user = $this->getUser();
 
@@ -64,5 +69,89 @@ class UserController extends AbstractController
 
                 $this->addFlash('success', 'Un email de vérification a été envoyé à votre adresse email.');
                 return $this->redirectToRoute('user.profile');
+        }
+
+        #[Route('/user/update/email', name: 'user.update_email')]
+        public function formForEmail(Request $request, EntityManagerInterface $em): Response
+        {
+                $form = $this->createForm(UpdateEmailFormType::class, $this->getUser());
+                $form->handleRequest($request);
+
+                if(!$this->getUser()) {
+                        $this->addFlash('error', 'Vous devez être connecté pour accéder à cette page.');
+                        return $this->redirectToRoute('app_login');
+                }
+
+                if($form->isSubmitted() && $form->isValid()) {
+                        $em->flush();
+                        $this->addFlash('success', 'Votre adresse email a été modifiée.');
+
+                        if($this->getUser()->isVerified()) {
+                                $this->getUser()->setIsVerified(false);
+                                $em->flush();
+                                return $this->redirectToRoute('user.send_email_verification');
+                        }
+
+                        // ensure we send a verification email if the user's email is not verified and they changed it
+                        return $this->redirectToRoute('user.send_email_verification');
+                }
+
+                return $this->render('user/profile-email-update.html.twig', [
+                        'form' => $form->createView(),
+                        'user' => $this->getUser()
+                ]);
+        }
+
+        #[Route('/user/update/password', name: 'user.update_password')]
+        public function formForPassword(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $em): Response
+        {
+                $form = $this->createForm(UpdatePasswordFieldType::class, $this->getUser());
+                $form->handleRequest($request);
+
+                if($this->getUser()) {
+                        $user = $this->getUser();
+                } else {
+                        $this->addFlash('error', 'Vous devez être connecté pour accéder à cette page.');
+                        return $this->redirectToRoute('app_login');
+                }
+
+                if($user && $form->isSubmitted() && $form->isValid()) {
+                        // encode the plain password
+                        $user->setPassword(
+                            $userPasswordHasher->hashPassword(
+                                $user,
+                                $form->get('password')->getData()
+                            )
+                        );
+                        $em->flush();
+                        $this->addFlash('success', 'Votre mot de passe a été modifié.');
+                        return $this->redirectToRoute('user.profile');
+                }
+
+                return $this->render('user/profile-password-update.html.twig', [
+                        'form' => $form->createView(),
+                        'user' => $this->getUser()
+                ]);
+        }
+
+        #[Route('/user/update/profile', name: 'user.update_profile')]
+        public function formForProfile(): Response
+        {
+                $form = $this->createForm(UpdateProfileFormType::class, $this->getUser());
+
+                return $this->render('user/profile-update.html.twig', [
+                        'form' => $form->createView(),
+                        'user' => $this->getUser()
+                ]);
+        }
+
+        #[Route('/user/delete', name: 'user.delete')]
+        public function deleteUser(EntityManagerInterface $em): RedirectResponse
+        {
+                $em->remove($this->getUser());
+                $em->flush();
+
+                $this->addFlash('success', 'Votre compte a été supprimé.');
+                return $this->redirectToRoute('app_logout');
         }
 }
